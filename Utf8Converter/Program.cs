b5e795d0e14296f5f6ed91fc361adc7116b80038
Pierre Arnaud <arnaud@epsitec.ch>
2016-05-26 10:20:34 +0200

    Initial version, building and running with 'dotnet run'.

--- Utf8Converter/Program.cs
+++ Utf8Converter/Program.cs
@@ -1,14 +1,44 @@
-﻿using System;
+//	Copyright © 2016, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Utf8Converter
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-        }
-    }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			string filter = args.Length > 1 ? args[1] : "*.cs";
+			string root   = args.Length > 2 ? args[2] : ".";
+
+			foreach (var path in System.IO.Directory.GetFiles (root, filter, System.IO.SearchOption.AllDirectories))
+			{
+				if (Program.ProcessFile (path))
+				{
+					System.Console.WriteLine ("{0}: converted", path);
+				}
+			}
+		}
+
+
+		public static bool ProcessFile(string path)
+		{
+			var input  = System.IO.File.ReadAllBytes (path);
+			var output = System.Text.Encoding.UTF8.GetBytes (UnicodeSampler.GetUTF8String (input));
+
+			if (input.SequenceEqual (output))
+			{
+				return false;
+			}
+			else
+			{
+				System.IO.File.WriteAllBytes (path, output);
+				return true;
+			}
+		}
+
+	}
 }
---
+++ Utf8Converter/UnicodeSampler.cs
@@ -0,0 +1,164 @@
+//	Copyright © 2016, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+namespace Utf8Converter
+{
+	public static class UnicodeSampler
+	{
+		static UnicodeSampler()
+		{
+			//	Configure Code Pages provider, so that we can resolve code page
+			//	1252 using System.Text.Encoding.GetEncoding (1252).
+
+			var provider = System.Text.CodePagesEncodingProvider.Instance;
+			System.Text.Encoding.RegisterProvider (provider);
+		}
+
+		public static bool IsValidUTF8(byte[] data)
+		{
+			int index = 0;
+			var length = data.Length;
+
+			while (index < length)
+			{
+				byte c = data[index++];
+
+				if (c <= 0x7F)
+				{
+					continue;
+				}
+
+				if (index >= length)
+				{
+					return false;
+				}
+
+				byte c1 = data[index++];
+
+				if ((c >= 0xC2) && (c <= 0xDF))
+				{
+					if ((c1 >= 0x80) && (c1 <= 0xBF))
+					{
+						continue;
+					}
+
+					return false;
+				}
+
+				if (index >= length)
+				{
+					return false;
+				}
+
+				byte c2 = data[index++];
+
+				if (c == 0xE0)
+				{
+					if ((c1 >= 0xA0) && (c1 <= 0xBF) &&
+						(c2 >= 0x80) && (c2 <= 0xBF))
+					{
+						continue;
+					}
+
+					return false;
+				}
+
+				if ((c >= 0xE1) && (c <= 0xEF))
+				{
+					if ((c1 >= 0x80) && (c1 <= 0xBF) &&
+						(c2 >= 0x80) && (c2 <= 0xBF))
+					{
+						continue;
+					}
+
+					return false;
+				}
+
+				if (index >= length)
+				{
+					return false;
+				}
+
+				byte c3 = data[index++];
+
+				if (c == 0xF0)
+				{
+					if ((c1 >= 0x90) && (c1 <= 0xBF) &&
+						(c2 >= 0x80) && (c2 <= 0xBF) &&
+						(c3 >= 0x80) && (c3 <= 0xBF))
+					{
+						continue;
+					}
+
+					return false;
+				}
+
+				if ((c >= 0xF1) && (c <= 0xF3))
+				{
+					if ((c1 >= 0x80) && (c1 <= 0xBF) &&
+						(c2 >= 0x80) && (c2 <= 0xBF) &&
+						(c3 >= 0x80) && (c3 <= 0xBF))
+					{
+						continue;
+					}
+
+					return false;
+				}
+
+				if (c == 0xF4)
+				{
+					if ((c1 >= 0x80) && (c1 <= 0x8F) &&
+						(c2 >= 0x80) && (c2 <= 0xBF) &&
+						(c3 >= 0x80) && (c3 <= 0xBF))
+					{
+						continue;
+					}
+
+					return false;
+				}
+
+				return false;
+			}
+
+			return true;
+		}
+
+		public static bool HasUtf8Bom(byte[] array)
+		{
+			//	This is a BOM, often added at the start of an UTF-8 file to identify it
+			//	as such on the Windows platform.
+
+			if ((array.Length >= 3) &&
+				(array[0] == 0xef) &&
+				(array[1] == 0xbb) &&
+				(array[2] == 0xbf))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public static string GetUTF8String(byte[] data)
+		{
+			if (UnicodeSampler.HasUtf8Bom (data))
+			{
+				//	Skip the BOM, keep only the UTF8 after it.
+				return System.Text.Encoding.UTF8.GetString (data, 3, data.Length - 3);
+			}
+			else if (UnicodeSampler.IsValidUTF8 (data))
+			{
+				//	Looks like legitimate UTF-8, so assume it really is UTF-8:
+				return System.Text.Encoding.UTF8.GetString (data, 0, data.Length);
+			}
+			else
+			{
+				//	Assume this is plain Windows ANSI Latin-1 (code page 1252)
+				var encoding = System.Text.Encoding.GetEncoding (1252);
+				return encoding.GetString (data, 0, data.Length);
+			}
+		}
+	}
+}
